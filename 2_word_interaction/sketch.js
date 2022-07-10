f01c79fdbbb6abc889e5e26d60da5139d8aa71b0
Michael Houmann <mhoumann87@gmail.com>
2022-07-10 13:38:18 +0200

    Update HTML and javascript

--- 2_word_interaction/sketch.js
+++ 2_word_interaction/sketch.js
@@ -15,5 +15,24 @@ function setup() {
 }
 
 const handleText = () => {
-  output.html(input.value());
+  // Get the input from the user
+  const str = input.value();
+  // Split the string into an array, the delimiter is anything not a word or number
+  const words = str.split(/(\W+)/);
+  console.log(words);
+  // Create a string with for all the words
+  let html = "";
+  // Create a HTML element (span) for each work
+  words.forEach((word) => {
+    if (/\W+/.test(word)) {
+      html += `${word}`;
+    } else {
+      html += `<span class="bg">${word}</span>`;
+    }
+  });
+
+  // Print the text on the page
+  output.html(html);
+
+  console.log(words);
 };
