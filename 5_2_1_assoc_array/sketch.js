6f83b2fd584d5f759cdb6aac5c5535cb49987e7f
Michael Houmann <mhoumann87@gmail.com>
2022-07-24 09:28:14 +0200

    Add new chapter. No code, only info

---
+++ 5_2_1_assoc_array/sketch.js
@@ -0,0 +1,10 @@
+/*
+  In an associative array we are working with keys and value pairs, witch makes it ideal
+  for working with counting.
+  {
+    "happy": 3,
+  }
+  Where happy are the word(key) and 3 are the number of times the word is used in the text (value).
+
+  It is easy to see that this is "just" a javascript object, or a JSON file.
+*/
