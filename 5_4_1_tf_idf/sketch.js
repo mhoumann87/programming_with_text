52593d58abbae3f492807e8fba06fdae83170b04
Michael Houmann <mhoumann87@gmail.com>
2022-07-25 12:29:09 +0200

    Add sample text, html and start of javascript

---
+++ 5_4_1_tf_idf/sketch.js
@@ -0,0 +1,39 @@
+/*
+  tf-idf read more on wikipedia https://en.wikipedia.org/wiki/Tf%E2%80%93idf
+  based on the coding train video https://youtu.be/RPMYV-eb6lI
+
+  The td-idf stands for  term frequency–inverse document frequency. It is a word counting algorithm
+  but instead of just counting the words (how many times does a term show up in a text), hold up against the
+  frequency af a word in other documents. 
+  
+  First we count the words in a document, then we compare this count with other documents, and if a word
+  is frequent in these as well, the word gets a low score.
+  
+  It is a way to find keywords that are "unique" for this document.
+
+*/
+
+// Variables for the elements on the page
+let upload, submit, output;
+// A variable for oor text files
+let txt = [];
+// A variable for our words in the text as an object
+let wordCounts = {};
+// An array of files we use for reference
+const files = [
+  'files/eclipse.txt',
+  'files/fish.txt',
+  'files/rainbow.txt',
+  'files/sports.txt',
+];
+
+// We preload the files we want to compare up against
+function preload() {
+  // As we have an array we have to go over every file in there
+  for (let i = 0; i < files.length; i++) {
+    txt[i] = loadStrings(files[i]);
+  }
+}
+function setup() {
+  console.log(txt);
+}
